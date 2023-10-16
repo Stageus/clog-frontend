30f549ec9aadd48e8d209688d4132ac01a6e0fa0
김영제 <kim-yeongje@Yeongjes-MacBook-Pro.local>
2023-10-17 01:23:42 +0900

    오류 복구

--- src/components/Container/Club/Club_Main_Section.js
+++ src/components/Container/Club/Club_Main_Section.js
@@ -5,6 +5,8 @@ import Club_ClubProfileBox from "./Club_ClubProfileBox"
 import Club_BoardList from "./Club_BoardList"
 
 // import recoil
+import { useRecoilValue, useSetRecoilState, useRecoilState } from "recoil"
+import { clubInfoAtom, userClubProfileAtom } from "../../../recoil/ClubAtom"
 
 // import styled
 import styled from "styled-components"
@@ -12,29 +14,67 @@ import { Flexdiv } from "../../../style/common"
 
 // import image
 
+const Main = styled.main`
+    width : 1080px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+`
+
+const Nav = styled.nav`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+`
+
+const Section = styled.section`
+    width: 860px;
+`
+
+const H1 = styled.h1`
+    margin: ${props => props.margin || "none"};
+    padding: ${props => props.padding || "none"};
+    font-size: ${props => props.fontSize || "24px"};
+    font-family: ${props => props.fontFamily || "'Noto Sans KR', sans-serif"};
+    font-weight: ${props => props.fontBold || "800"};
+    color: ${props => props.color || "#000000"};
+`
+
+
 const Club_Main_Section = () => {
 	// route ============================================================
 
     // props ============================================================
 
     // state ============================================================
+    const club = useRecoilState(clubInfoAtom)
 
     // event ============================================================
 
 
     return(
         <React.Fragment>
-            <Flexdiv flex="0_0_auto_row_center_flex-start" position="relative_70px">
-                <Flexdiv flex="0_0_auto_row_space-between_flex-start" width="1080px">
-                    <Flexdiv flex="0_0_auto_column_flex-start-center">
+            <Flexdiv flex="0_0_auto_row_center_flex-start" position="relative_70px" cursor="default">
+                <Main>
+                    <Nav>
                         <Club_ClubProfileBox/>
                         <Club_BoardList/>
-                    </Flexdiv>
+                    </Nav>
 
-                    <Flexdiv width="860px" height="600px" backgroundColor="#F0F0F0">
+                    {/* 여기서부터 main section */}
+                    <Section>
+                        <Flexdiv height="30px" margin="0 0 20px 0" flex="0_0_auto_raw_flex-start_center">
+                            <H1>전체 글 보기</H1>
+                        </Flexdiv>
 
-                    </Flexdiv>
-                </Flexdiv>
+                        <Flexdiv width="860px" margin="0 0 50px 0" flex="0_0_auto_column_center_center">
+                            <Flexdiv width="860px" height="50px" borderTop={"1px solid #" + club[0].themeColor}>
+
+                            </Flexdiv>
+                        </Flexdiv>
+                    </Section>
+                </Main>
             </Flexdiv>
 
         </React.Fragment>
